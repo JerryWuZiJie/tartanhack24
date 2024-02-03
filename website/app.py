198b8813e5f4ccfeeca0956f1cfeb4ad25423a96
Jerry Wu <jerrywuzijie@gmail.com>
2024-02-03 16:40:47 -0500

    Implement detail showing of webpage

--- website/app.py
+++ website/app.py
@@ -1,33 +1,84 @@
-# Bangjie Xue
-from flask import Flask, render_template, request
-import urllib.parse
-import os
 import pandas as pd
-from pprint import pprint
-import numpy as np
+from flask import Flask, render_template, request
 from sklearn.manifold import TSNE
 from scipy.stats import spearmanr
 
+
 def scoring(rank, df):
-    
+
     def tsne_score(df):
-        TSNEout = TSNE(n_components=2, method='exact').fit_transform(df)
+        TSNEout = TSNE(n_components=2, method="exact").fit_transform(df)
         score = TSNEout[:, 0] - TSNEout[:, 1]
 
         return score
-    
-    df = (df - df.min(0))/(df.max(0) - df.min(0))
-    
+
+    df = (df - df.min(0)) / (df.max(0) - df.min(0))
+
     #  spearsman corr
     corr = spearmanr(rank, df)[0][0][1:]
     df *= corr
-    
+
     score = tsne_score(df)
-    
-    
+
     return corr, score
 
+
 app = Flask(__name__)
+
+df = pd.read_csv("../Datasets/post_processed.csv")
+# Normalize the data sum to 1
+for col in df.iloc[:, 3:]:
+    if pd.api.types.is_numeric_dtype(df[col]):
+        df[col] = df[col] / df[col].sum()
+    if df[col].dtype == object and df[col].str.contains("%").any():
+        # Remove the percentage sign and convert to float
+        df[col] = df[col].str.replace("%", "").astype(float) / 100
+        # Now normalize
+        df[col] = df[col] / df[col].sum()
+    if df[col].dtype == object and df[col].str.contains("$").any():
+        # Remove the percentage sign and convert to float
+        df[col] = (
+            df[col].str.replace("[\$,]", "", regex=True).astype(float) / 100
+        )
+        # Now normalize
+        df[col] = df[col] / df[col].sum()
+# Data Selection
+drop_lst = [
+    "SNAP_All_csv_Median_Home_Value_",
+    "Med__Val____00_in__10_Dollars_",
+    "SNAP_All_csv_Part_1_Crime_per_1",
+    "SNAP_All_csv__Murder__2010_",
+    "SNAP_All_csv__Rape__2010_",
+    "SNAP_All_csv__Robbery__2010_",
+    "F_Agr__Assault__2010_",
+    "SNAP_All_csv__Burglary__2010_",
+    "SNAP_All_csv__Auto_Theft__2010_",
+    "SNAP_All_csv__Drug_Violations__",
+    "SNAP_All_csv___Good___Excellent",
+    "SNAP_All_csv___Average_Conditio",
+    "SNAP_All_csv___Poor___Derelict_",
+    "SNAP_All_csv_Landslide_Prone___",
+    "SNAP_All_csv_Woodland____of_lan",
+    "SNAP_All_csv_Cemetery____of_lan",
+    "Population (2010)",
+    "Miles of Major Roads",
+    "Total Street Miles",
+    "Commute to Work: Drive Alone (2010)",
+    "Commute to Work: Carpool/Vanpool (2010)",
+    "Commute to Work: Public Transportation (2010)",
+    "Commute to Work: Taxi (2010)",
+    "Commute to Work: Motorcycle (2010)",
+    "Commute to Work: Bicycle (2010)",
+    "Commute to Work: Walk (2010)",
+    "Commute to Work: Other (2010)",
+    "Work at Home (2010)",
+    "Neighborhood_2010_SQMILES",
+    "SNAP_All_csv_2009_Median_Income",
+    "Total Working Pop. (Age 16+) (2010)",
+    "SNAP_All_csv_Residential",
+]
+df.drop(columns=drop_lst, inplace=True)
+df.drop(columns=["Latitude", "Longitude"], inplace=True)
 
 
 parameters = ["0", "0", "0", "0", "0"]
@@ -47,69 +98,37 @@ def index():
     rank = ["Irrelevant", "Low", "Medium", "High"]
     if request.method == "POST":
         parameters = [request.form.get(param_name) for param_name in param_names]
-        mapped_param = {
-            '0':0,
-            '1':0.25,
-            '2':0.6,
-            '3':1
-            }
-        
-        df = pd.read_csv("../Datasets/post_processed.csv")
-        # Normalize the data sum to 1
-        for col in df.iloc[:, 3:]:
-            if pd.api.types.is_numeric_dtype(df[col]):
-                df[col] = df[col] / df[col].sum()
-            if df[col].dtype == object and df[col].str.contains('%').any():
-                # Remove the percentage sign and convert to float
-                df[col] = df[col].str.replace('%', '').astype(float) / 100
-                # Now normalize
-                df[col] = df[col] / df[col].sum()
-            if df[col].dtype == object and df[col].str.contains('$').any():
-                # Remove the percentage sign and convert to float
-                df[col] = df[col].str.replace('[\$,]', '', regex=True).astype(float) / 100
-                # Now normalize
-                df[col] = df[col] / df[col].sum()
-        # Data Selection
-        drop_lst = ['SNAP_All_csv_Median_Home_Value_','Med__Val____00_in__10_Dollars_','SNAP_All_csv_Part_1_Crime_per_1',
-                'SNAP_All_csv__Murder__2010_',
-               'SNAP_All_csv__Rape__2010_', 'SNAP_All_csv__Robbery__2010_',
-               'F_Agr__Assault__2010_', 'SNAP_All_csv__Burglary__2010_',
-               'SNAP_All_csv__Auto_Theft__2010_', 'SNAP_All_csv__Drug_Violations__', 'SNAP_All_csv___Good___Excellent', 'SNAP_All_csv___Average_Conditio',
-               'SNAP_All_csv___Poor___Derelict_', 'SNAP_All_csv_Landslide_Prone___', 'SNAP_All_csv_Woodland____of_lan', 'SNAP_All_csv_Cemetery____of_lan','Population (2010)', 'Miles of Major Roads',
-               'Total Street Miles','Commute to Work: Drive Alone (2010)',
-               'Commute to Work: Carpool/Vanpool (2010)',
-               'Commute to Work: Public Transportation (2010)',
-               'Commute to Work: Taxi (2010)', 'Commute to Work: Motorcycle (2010)',
-               'Commute to Work: Bicycle (2010)', 'Commute to Work: Walk (2010)',
-               'Commute to Work: Other (2010)', 'Work at Home (2010)','Neighborhood_2010_SQMILES','SNAP_All_csv_2009_Median_Income','Total Working Pop. (Age 16+) (2010)', 'SNAP_All_csv_Residential',]
-        trash_lst = df[drop_lst]
-        df.drop(columns=drop_lst, inplace=True)
-        df.drop(columns=['Latitude', 'Longitude'], inplace=True)
-        # Parking friendliness
-        # (df.iloc[:, 12] + df.iloc[:, 13] - df.iloc[:, 14])/3
-        # Bike friendliness
-        # df.iloc[:, 2]
-        # Ease of Commute
-        # (df.iloc[:, 15] + df.iloc[:, 16])/2
-        # Affordability
-        # (-df.iloc[:, 5] + -df.iloc[:, 6] + df.iloc[:, 7] - df.iloc[:, 8])/4
-        # Safety
-        # (-df.iloc[:, 1] -df.iloc[:, 3] -df.iloc[:, 4] - df.iloc[:, 9] - df.iloc[:, 10] - df.iloc[:, 11])/6
+        mapped_param = {"0": 0, "1": 0.25, "2": 0.6, "3": 1}
 
-        df["sum"] = mapped_param[parameters[0]]*(df.iloc[:, 12] + df.iloc[:, 13])/2 +\
-            mapped_param[parameters[1]]*df.iloc[:, 2] +\
-                mapped_param[parameters[2]]*(df.iloc[:, 15] + df.iloc[:, 16])/2 +\
-                    mapped_param[parameters[3]]*(-df.iloc[:, 5] + -df.iloc[:, 6] + df.iloc[:, 7] - df.iloc[:, 8])/4 +\
-                        mapped_param[parameters[4]]*(-df.iloc[:, 1] -df.iloc[:, 3] -df.iloc[:, 4] - df.iloc[:, 9] - df.iloc[:, 10] - df.iloc[:, 11])/6
+        df["sum"] = (
+            mapped_param[parameters[0]] * (df.iloc[:, 12] + df.iloc[:, 13]) / 2
+            + mapped_param[parameters[1]] * df.iloc[:, 2]
+            + mapped_param[parameters[2]] * (df.iloc[:, 15] + df.iloc[:, 16]) / 2
+            + mapped_param[parameters[3]]
+            * (-df.iloc[:, 5] + -df.iloc[:, 6] + df.iloc[:, 7] - df.iloc[:, 8])
+            / 4
+            + mapped_param[parameters[4]]
+            * (
+                -df.iloc[:, 1]
+                - df.iloc[:, 3]
+                - df.iloc[:, 4]
+                - df.iloc[:, 9]
+                - df.iloc[:, 10]
+                - df.iloc[:, 11]
+            )
+            / 6
+        )
 
-        print(df.iloc[:,[0,-1]].values)
-        df.sort_values('sum', ascending=False, inplace = True)
-        # print(df.iloc[:,0].values)
-        ranked_list = df.iloc[:,0].values
-        
-        # ranked_list = ['one', 'two', 'three', 'four', 'five'] 
-        
-        return render_template('index.html', parameters = parameters, ranked_list = ranked_list, param_names = param_names, rank = rank)
+        df.sort_values("sum", ascending=False, inplace=True)
+        ranked_list = df.iloc[:, 0].values
+
+        return render_template(
+            "index.html",
+            parameters=parameters,
+            ranked_list=ranked_list,
+            param_names=param_names,
+            rank=rank,
+        )
     else:
         ranked_list = ["Please adjust the parameters and submit!"]
         return render_template(
@@ -126,12 +145,38 @@ def details(county):
     # Get all the information of this course from each table
     # TODO: detail_lst
     detail_lst = []
-    detail_lst.append(["crime rate", "0.5"])
-    detail_lst.append(["crimasdfe rate", "0.dasf5"])
-    detail_lst.append(["crime raefdte", "0.51"])
-    return render_template(
-        "details.html", county=county, table_list=detail_lst
-    )
+    
+    # df["sum"] = (
+    #         mapped_param[parameters[0]] * (df.iloc[:, 12] + df.iloc[:, 13]) / 2
+    #         + mapped_param[parameters[1]] * df.iloc[:, 2]
+    #         + mapped_param[parameters[2]] * (df.iloc[:, 15] + df.iloc[:, 16]) / 2
+    #         + mapped_param[parameters[3]]
+    #         * (-df.iloc[:, 5] + -df.iloc[:, 6] + df.iloc[:, 7] - df.iloc[:, 8])
+    #         / 4
+    #         + mapped_param[parameters[4]]
+    #         * (
+    #             -df.iloc[:, 1]
+    #             - df.iloc[:, 3]
+    #             - df.iloc[:, 4]
+    #             - df.iloc[:, 9]
+    #             - df.iloc[:, 10]
+    #             - df.iloc[:, 11]
+    #         )
+    #         / 6
+    #     )
+    
+    temp_df = df.loc[df["Neighborhood"] == county]
+    park_lst = ["Parking Friendliness", [temp_df.columns[12], temp_df.iloc[0, 12]], [temp_df.columns[13], temp_df.iloc[0, 13]]]
+    bike_lst = ["Biking Friendliness", [temp_df.columns[2], temp_df.iloc[0, 2]]]
+    commute_lst = ["Ease of Commute", [temp_df.columns[15], temp_df.iloc[0, 15]], [temp_df.columns[16], temp_df.iloc[0, 16]]]
+    afford_lst = ["Affordability", [temp_df.columns[5], temp_df.iloc[0, 5]], [temp_df.columns[6], temp_df.iloc[0, 6]], [temp_df.columns[7], temp_df.iloc[0, 7]], [temp_df.columns[8], temp_df.iloc[0, 8]]]
+    safe_lst = ["Safety", [temp_df.columns[1], temp_df.iloc[0, 1]], [temp_df.columns[3], temp_df.iloc[0, 3]], [temp_df.columns[4], temp_df.iloc[0, 4]], [temp_df.columns[9], temp_df.iloc[0, 9]], [temp_df.columns[10], temp_df.iloc[0, 10]], [temp_df.columns[11], temp_df.iloc[0, 11]]]
+    detail_lst.append(park_lst)
+    detail_lst.append(bike_lst)
+    detail_lst.append(commute_lst)
+    detail_lst.append(afford_lst)
+    detail_lst.append(safe_lst)
+    return render_template("details.html", county=county, table_list=detail_lst)
 
 
 if __name__ == "__main__":
